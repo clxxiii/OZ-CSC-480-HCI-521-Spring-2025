74100f6cb07cd58bcf85cd80d0335b6a8cf7d260
Logan <lsimons@oswego.edu>
2025-02-11 21:27:46 -0500

    Added backend directory via upload

---
+++ _backend/services/quote-service/src/main/java/com/RestApplication.java
@@ -0,0 +1,8 @@
+package com;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/quotes")
+public class RestApplication extends Application {
+}
---
+++ _backend/services/user-service/README.txt
@@ -0,0 +1,5 @@
+This is the account-service documentation.
+
+In order to build and deploy this service, follow the steps:
+
+1. run build.bat
