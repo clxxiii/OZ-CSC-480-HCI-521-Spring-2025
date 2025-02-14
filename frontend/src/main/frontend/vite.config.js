97c463cfc103962767ad1edd5545b7e9387a5288
SamanthaJeanneb <123563481+SamanthaJeanneb@users.noreply.github.com>
2025-02-13 20:02:03 -0500

    Routing fix + name update

--- frontend/src/main/frontend/vite.config.js
+++ frontend/src/main/frontend/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, 
+  },
   build: {
     outDir: "out",
     emptyOutDir: true
+  },
+  preview: {
+    port: 5173, 
+    strictPort: true, 
   }
-})
+});
