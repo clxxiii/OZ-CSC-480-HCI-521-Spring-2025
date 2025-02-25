fcaeb5210b1aefdf37b483553e5296147403c269
Matthew Sikorski <sikomattrski@gmail.com>
2025-02-24 20:48:54 -0500

    Changes to Navigation Bar

--- frontend/src/main/frontend/src/components/TopNavigation.jsx
+++ frontend/src/main/frontend/src/components/TopNavigation.jsx
@@ -14,27 +14,42 @@ const TopNavigation = ({ user }) => {
     fontSize: '24px'
   };
 
-
+  //
+  //  Upload Quote currently does nothing. 
+  // 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-        <Link className="navbar-brand pl-2" to="/">Home</Link>
+        <Link className="navbar-brand pl-2" to="/">Logo</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse pr-2" id="navbarNav">
         <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
           {!user && (
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
+            <>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
+            </>
           )}
           {user && (
-            <li className="nav-item">
-              <div style={circleStyle}>
-                <i className="bi bi-person"></i>
-              </div>
-              {user.Username}
-            </li>
+            <>
+              <li className="nav-item">
+                <Link className="nav-link">Upload Quote</Link> 
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/saved-quotes">Saved</Link>
+              </li>
+              <li className="nav-item">
+                <div style={circleStyle}>
+                  <i className="bi bi-person"></i>
+                </div>
+                {user.Username}
+              </li>
+            </>
           )}
         </ul>
       </div>
